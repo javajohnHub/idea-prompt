01dfd0cc4b36e1aef0f40dc43c676dcfbbae9b6a
John Edwards <joedw1978@gmail.com>
2021-07-10 18:09:21 -0400

    auth

--- client/src/app/prompt copy/prompt.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-prompt',
-  templateUrl: './prompt.component.html',
-  styleUrls: ['./prompt.component.scss'],
-})
-export class PromptComponent {}
