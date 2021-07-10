71e30d07c5c3fd5c73099bc8e348b83d72b9022f
John Edwards <joedw1978@gmail.com>
2021-07-10 19:31:27 -0400

    auth

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+// import { AuthGuard } from './auth.guard';
 import { LogoutComponent } from './logout/logout.component';
 import { PromptComponent } from './prompt/prompt.component';
 import { SettingsComponent } from './settings/settings.component';
