4b860a37c098cfeca082f3a0d83eaa5d177e17b3
John Edwards <joedw1978@gmail.com>
2021-07-10 19:47:59 -0400

    auth

--- client/src/app/prompt/prompt.component.ts
+++ client/src/app/prompt/prompt.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-prompt',
   templateUrl: './prompt.component.html',
   styleUrls: ['./prompt.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PromptComponent {
   constructor(public auth: AuthService) {}
