d265b3beb414b0ece5d82ccac07684bef3a740b7
John Edwards <joedw1978@gmail.com>
2021-07-10 21:43:10 -0400

    trying auth0 starter

--- server/sockets.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (io) {
-  io.sockets.on("connection", function (socket) {
-    console.log("connected", socket.id);
-
-    socket.on("get cards", () => {
-      console.log("get cards");
-    });
-  });
-};
