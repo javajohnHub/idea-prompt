e38c88731efd06fac2bf78b91a39812654775fae
John Edwards <joedw1978@gmail.com>
2021-07-10 20:38:59 -0400

    auth

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendfile('dist/index.html');
+router.get("/", function (req, res, next) {
+  res.sendfile("dist/index.html");
 });
 
+router.get("/authorize", function (req, res, next) {
+  console.log("hit");
+});
 module.exports = router;
